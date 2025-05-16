f294963dbb5c803388d9f377358869cc3a14ff1a
Malshii <kulasinghe1malshi@gmail.com>
2025-05-16 08:57:07 +0530

    fix build issue

--- components/ClarityScript.jsx
+++ components/ClarityScript.jsx
@@ -1,28 +1,28 @@
-// app/components/ClarityScript.jsx
 'use client';
 
-import { useEffect } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
+import React, { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
+import Script from 'next/script';
 
 export default function ClarityScript() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
-  
-  useEffect(() => {
-    // Paste your Microsoft Clarity code here
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "rk8ue5lw7f");
-  }, []);
   
   // Track route changes
   useEffect(() => {
     if (window.clarity) {
       window.clarity("newPage");
     }
-  }, [pathname, searchParams]);
+  }, [pathname]);
   
-  return null;
+  return (
+    <Script id="microsoft-clarity" strategy="afterInteractive">
+      {`
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "rk8ue5lw7f");
+      `}
+    </Script>
+  );
 }
