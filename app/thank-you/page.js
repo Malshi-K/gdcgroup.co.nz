fb26fbf6c34a0e09b2ee32de3261e819918c24d6
Malshii <kulasinghe1malshi@gmail.com>
2026-05-19 09:47:05 +0530

    fixed build issue

--- app/thank-you/page.js
+++ app/thank-you/page.js
@@ -1,11 +1,12 @@
 "use client";
+import Link from "next/link";
 
 export default function ThankYouPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[60vh] bg-white text-black p-8">
       <h1 className="text-2xl md:text-3xl font-bold text-customBlue mb-4">Thank you for your message!</h1>
       <p className="text-lg text-gray-700 mb-6">We appreciate you contacting GDC Consultants Ltd. Our team will get back to you as soon as possible.</p>
-      <a href="/" className="text-customBlue underline hover:text-customYellow">Return to Home</a>
+      <Link href="/" className="text-customBlue underline hover:text-customYellow">Return to Home</Link>
     </div>
   );
 }
