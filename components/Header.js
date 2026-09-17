3312a5fd90cf2b9bebea7857c3aa3449eda52a61
Malshii <kulasinghe1malshi@gmail.com>
2026-09-17 07:54:23 +0530

    removed our team page

--- components/Header.js
+++ components/Header.js
@@ -174,7 +174,6 @@ const Header = () => {
                   { href: "/locations", label: "Our Locations" },
                 ],
               },
-              {/* { href: "/team", label: "OUR TEAM" }, */}
               { href: "/blogs", label: "BLOGS" },
             ].map((item) =>
               item.items ? (
@@ -321,7 +320,6 @@ const Header = () => {
                   { href: "/locations", label: "Our Locations" },
                 ],
               },
-              {/* { href: "/team", label: "Our Team" }, */}
               { href: "/blogs", label: "Blogs" },
               { href: "tel:+6478380090", label: "Call Now" },
               { href: "/contact-us", label: "Our Locations" },
